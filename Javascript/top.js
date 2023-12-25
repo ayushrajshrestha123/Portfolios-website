284e8b3bd310dd8815a3a2159aee5afda23364e3
ayushrajshrestha123 <126042841+ayushrajshrestha123@users.noreply.github.com>
2023-12-25 23:03:27 +0545

    Initial commit

---
+++ Javascript/top.js
@@ -0,0 +1,8 @@
+
+
+    // When the user clicks on the button, scroll to the top of the document
+    function topFunction() {
+     
+        document.documentElement.scrollTop = 0;
+        
+    }
